f2b8efb662717aa6399ba6ca9f2a9767bec60e90
Alexandra Malaise <alexandra.malaise@outlook.fr>
2018-04-04 20:17:06 +0200

    Phase 3

--- db/migrate/20180404180343_create_users.rb
+++ db/migrate/20180404180343_create_users.rb
@@ -4,9 +4,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :pseudo
       t.integer :user
       t.string :email
-      t.string :photo
       t.string :profile
-      t.string :text
+      t.string :description
 
       t.timestamps
     end
